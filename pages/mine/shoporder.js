f38be8c4c62481e6f0d22451dba0fa1e1e0866ef
l00251692 <982679298@qq.com>
2018-02-10 02:51:32 +0800

    商家后台websocket连接，接收订单通知

--- pages/mine/shoporder.js
+++ pages/mine/shoporder.js
@@ -9,6 +9,8 @@ import {
 } from '../../utils/util'
 
 var initData = {
+  websocketFlag:false,
+  tip: '',
   page: 0,
   hasMore: true,
   loading: false,
@@ -17,11 +19,11 @@ var initData = {
 
 Page({
   data: {
-    ORDER_STATES
+    ORDER_STATES,
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    console.log('onLoad')
+    console.log('min load')
     this.id = options.id
     var that = this
     if (getApp().globalData.loginInfo.is_login) {
@@ -39,10 +41,14 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
+    this.setData({
+      websocketFlag: false
+    })
   },
   initData(cb) {
     this.setData(initData)
     this.loadData(cb)
+    this.connectWebsocket()
   },
   loadData(cb) {
     var that = this
@@ -79,6 +85,62 @@ Page({
       }
     })
   },
+
+  connectWebsocket: function () {
+    var that = this
+    var websocketFlag  = wx.getStorageSync('websocketFlag')
+    if (websocketFlag)
+    {
+      this.setData({
+        tip:'您已登录商家系统，请保持小程序不要关闭'
+      })
+      return
+    }
+    var { user_id, user_token } = getApp().globalData.loginInfo.userInfo
+    wx.connectSocket({
+      url: 'ws://localhost/webSocketServer?x=' + user_id,
+      data: {
+        y:'',
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: "GET"
+    })
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！')
+      wx.setStorageSync('websocketFlag', true)
+      that.setData({
+        tip:'您已登录商家系统，请保持小程序不要关闭'
+      })
+    })
+    wx.onSocketError(function (res) {
+      console.log('WebSocket连接打开失败，请检查！')
+      wx.setStorageSync('websocketFlag', false)
+    })
+    wx.onSocketMessage(function (res) {
+  
+      if (res.data == '连接成功')
+      {
+         console.log('连接成功')
+      }
+      else if (res.data == '新订单')
+      {
+          wx.playBackgroundAudio({
+            //播放地址
+            dataUrl: 'http://localhost/order.mp3',
+            title: '积木',
+            coverImgUrl: ''
+          })
+          that.setData({
+            tip: '您有新的订单，请及时处理'
+          })
+          that.loadData()        
+      }
+      
+    })
+  },
+
   onRecvOrderTap(e) {
     var { id } = e.currentTarget
     var that = this
@@ -94,15 +156,17 @@ Page({
     setRecvOrder({
       order_id,
       success(data) {
-        
+        wx.stopBackgroundAudio()
         that.initData();
         that.setData({
-          loading: false
+          loading: false,
+          tip: '您已登录商家系统，请保持小程序不要关闭'
         })
       },
       error() {
         that.setData({
-          loading: false
+          loading: false,
+          tip: '订单未成功接收，请联系管理员'
         })
       }
     })
@@ -123,15 +187,17 @@ Page({
     setRejectOrder({
       order_id,
       success(data) {
-
+        wx.stopBackgroundAudio()
         that.initData();
         that.setData({
-          loading: false
+          loading: false,
+          tip: '您已登录商家系统，请保持小程序不要关闭'
         })
       },
       error() {
         that.setData({
-          loading: false
+          loading: false,
+          tip: '订单未成功拒绝，请联系管理员'
         })
       }
     })
---
+++ pages/project/mine.js
@@ -0,0 +1,63 @@
+import {
+  getMyProjectList
+} from '../../utils/api'
+
+Page({
+  data: {
+    page: 0,
+    hasMore: true,
+    loading: false
+  },
+    onLoad: function () {
+      var that = this
+      this.init()  
+    },
+    onShow: function () {
+      // 页面显示
+    },
+    init()
+    {
+      var that = this
+      this.initData()
+      this.getMyProjectList()
+    },
+    initData() {
+      this.setData({
+        page: 0,
+        hasMore: true,
+        loading: false,
+        projectList: null
+      })
+    },
+    
+    getMyProjectList: function (e) { 
+      if (this.data.loading) {
+        return;
+      }
+      var that = this;
+      var { page } = this.data
+      getMyProjectList(
+      {
+          page,
+          success(data) {
+            var list  = data.list
+            var { projectList} = that.data
+            that.setData({
+              projectList: projectList ? projectList.concat(list) : list,
+              page: page + 1,
+              hasMore: data.count == 10, //一次最多10个，如果这次取到10个说明还有
+              loading: false
+            })
+          }
+      })
+    },
+    onReachBottom(e) {
+       if (this.data.hasMore && !this.data.loading) {
+         this.getMyProjectList();
+       }
+    },
+    
+    callback() {
+       this.init()
+    },
+})
